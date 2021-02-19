c0f6173ffd091b653ab45d0fa7acddecfe114605
Radek M <46869083+crushyna@users.noreply.github.com>
2021-02-19 12:23:38 +0100

    works in local development

--- MLServer/__init__.py
+++ MLServer/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import random
+import json
 
 import azure.functions as func
 
@@ -6,19 +8,23 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    input_data = req.params.get('data')
+    if not input_data:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            input_data = req_body.get('data')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if input_data:
+        animals = ['cat', 'dog', 'unknown']
+        answer = random.choice(animals)
+
+        return func.HttpResponse(json.dumps({'message': answer, 'status': 'success'}),
+                                 mimetype="application/json")
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse(json.dumps({'message': 'improper or no input data', 'status': 'failure'}),
+                                 mimetype="application/json", status_code=400)
+        # "This HTTP triggered function executed successfully. "
+        # "Pass a name in the query string or in the request body for a personalized response.",

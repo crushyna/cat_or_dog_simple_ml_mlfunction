9b86cc68d4fcb34c9ce1fbe54fa8690156c5b009
Radek M <46869083+crushyna@users.noreply.github.com>
2021-02-21 17:00:01 +0100

    dockerfile updated, entry point ready to accept images

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # To enable ssh & remote debugging on app service change the base image to the one below
-FROM mcr.microsoft.com/azure-functions/python:3.0-python3.9-appservice
+FROM mcr.microsoft.com/azure-functions/python:3.0-python3.8-appservice
 # FROM mcr.microsoft.com/azure-functions/python:3.0-python3.9
 
 ENV AzureWebJobsScriptRoot=/home/site/wwwroot \
@@ -8,6 +8,7 @@ ENV AzureWebJobsScriptRoot=/home/site/wwwroot \
 ENV CURRENT_MODEL catordog_model_07_.h5
 
 COPY requirements.txt /
+RUN python -m pip install --upgrade pip
 RUN pip install -r /requirements.txt
 
 COPY . /home/site/wwwroot
--- MLServer/__init__.py
+++ MLServer/__init__.py
@@ -1,43 +1,82 @@
+import io
 import logging
-import os
+import os.path
 import json
+import sys
 import azure.functions as func
 import tensorflow as tf
-from tensorflow import expand_dims
-from tensorflow.keras.models import load_model
 from numpy import argmax
-
+from PIL import Image
+from datetime import datetime
 
 MODELS_DIR = 'models'
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    """
+    Entry point for ML functions.
+    Requires image to be send in request body.
+    Image is then saved locally, and predicted using pre-trained model.
+
+    !!! IMPORTANT !!!
+    File must have dimensions 160x160x3. Otherwise it won't work correctly!
+
+    :param req: func.HttpRequest
+    :return: func.HttpResponse
+    """
     logging.info('Python HTTP trigger function processed a request.')
+    base_64_image_bytes = req.get_body()
+    if base_64_image_bytes:
+        # Get current time data
+        timestamp = datetime.now()
+        timestamp = timestamp.strftime('%Y%m%d%H%M%S')
 
-    input_data = req.params.get('data')
-    if not input_data:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            input_data = req_body.get('data')
+        # Check image
+        file_size = sys.getsizeof(base_64_image_bytes)
+        logging.info(f"Received image of size: {file_size} bytes")
 
-    if input_data:
-        # animals = ['cat', 'dog', 'unknown']
-        # answer = random.choice(animals)
+        # Save image locally
+        stream = io.BytesIO(base_64_image_bytes)
+        img = Image.open(stream)
+        temp_filename = f"temp_image{timestamp}.jpg"
+        img.save(temp_filename)
+        logging.info(f"File saved: {os.path.isfile(temp_filename)}")
+
+        # Check dimensions
+        img2 = Image.open(temp_filename)
+        width, height = img2.size
+        if width or height != 160:
+            return func.HttpResponse(json.dumps({'message': 'input image has incorrect dimensions',
+                                                 'status': 'error'}),
+                                     mimetype="application/json", status_code=400)
+
+        logging.info(f"Saved file dimensions: {width} x {height}")
+
+        # Load with Tensorflow
+        logging.info("Loading image with Tensorflow")
+        img = tf.keras.preprocessing.image.load_img(temp_filename, target_size=(height, width))
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)
+
+        # Load model
+        logging.info("Loading Tensorflow model")
+        model = tf.keras.models.load_model('catordog_model_07_.h5')
+
+        # Make predictions
+        logging.info("Making predictions")
         class_names = ['cat', 'dog']
-        model = load_model(os.path.join(MODELS_DIR, os.getenv('CURRENT_MODEL')))
-        img_array = expand_dims(input_data, 0)
         predictions = model.predict(img_array)
         score = tf.nn.softmax(predictions[0])
-        return func.HttpResponse(json.dumps({'message': class_names[argmax(score)],
-                                             'data': score.numpy(),
+
+        # Remove temporary file
+        os.remove(temp_filename)
+
+        return func.HttpResponse(json.dumps({'message': str(class_names[argmax(score)]),
+                                             'data': str(score.numpy()),
                                              'status': 'success'}),
-                                 mimetype="application/json")
+                                 mimetype="application/json", status_code=200)
+
     else:
         return func.HttpResponse(json.dumps({'message': 'improper or no input data',
-                                             'status': 'failure'}),
+                                             'status': 'error'}),
                                  mimetype="application/json", status_code=400)
-        # "This HTTP triggered function executed successfully. "
-        # "Pass a name in the query string or in the request body for a personalized response.",

70b7623d7d3d6ee8920dacb5d8f18130620c5b11
Radek M <46869083+crushyna@users.noreply.github.com>
2021-02-22 21:33:25 +0100

    updated for "with open" method

--- MLServer/__init__.py
+++ MLServer/__init__.py
@@ -36,10 +36,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.info(f"Received image of size: {file_size} bytes")
 
         # Save image locally
-        stream = io.BytesIO(base_64_image_bytes)
-        img = Image.open(stream)
+        # stream = io.BytesIO(base_64_image_bytes)
         temp_filename = f"temp_image{timestamp}.jpg"
-        img.save(temp_filename)
+        logging.info('Saving binary data...')
+        with open(temp_filename, 'wb') as img:
+            img.write(base_64_image_bytes)
+        img = Image.open(temp_filename)
+        # img.save(temp_filename)
         logging.info(f"File saved: {os.path.isfile(temp_filename)}")
 
         # Check dimensions
